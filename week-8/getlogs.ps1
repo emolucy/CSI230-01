60099eb29596ef5dbd8edc781679d082deedccf0
emolucy <lucy.accts@gmail.com>
2023-10-18 13:12:49 -0400

    First powershell script

---
+++ week-8/getlogs.ps1
@@ -0,0 +1,15 @@
+﻿cd C:\Users\champuser\Desktop
+
+clear
+
+Get-EventLog -list
+
+$readLog = Read-Host -Prompt "Please select a log to review"
+
+Write-Host "Exporting csv..."
+
+$exportPath = "C:\Users\champuser\Desktop\rep.csv"
+
+Get-EventLog -LogName $readLog | Export-Csv -NoTypeInformation -Path $exportPath
+
+Write-Host "Exported to $exportPath"
