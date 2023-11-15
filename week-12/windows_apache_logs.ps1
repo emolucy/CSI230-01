e08539b83a96e1db49f0139ffc1f394090c0edb1
emolucy <lucy.accts@gmail.com>
2023-11-15 13:42:47 -0500

    week12

---
+++ week-12/windows_apache_logs.ps1
@@ -0,0 +1,18 @@
+﻿$B = Get-Content C:\xampp\apache\logs\access.log | Select-String -Pattern ' 200 ' -NotMatch
+
+$regex = [regex]"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+
+$ips = $regex.Matches($B) | select @{Name="IP"; Expression={ $_.value }}
+
+$counts = $ips | Group-Object IP -NoElement
+
+$countsDetailed = $counts | select Count, Name, @{Name="Decision"; Expression={ if($_.Count -gt 3) {"Abnormal"} else {"Normal"} }}
+
+foreach($ip in $countsDetailed){
+    if([string]$ip.Decision -like "Abnormal") {
+        Write-Host "Blocking " $ip.Name
+        New-NetFirewallRule -DisplayName "Bad IPs $($ip.Name)" -Direction Inbound -Action Block -RemoteAddress $ip.Name
+    }
+}
+
+Remove-NetFirewallRule -DisplayName "Bad IPs*"
